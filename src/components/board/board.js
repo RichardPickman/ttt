57083a4c680121a506088744f515092fe4bc8a5e
RichardPickman <drezzerock@gmail.com>
2022-03-21 21:17:15 +0100

    fix: remove unnecessary code

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,20 +1,21 @@
 import { useState } from 'react';
-import React from 'react';
 
-import './board.css';
+import React from 'react';
 import Cells from '../cells/cells';
 import Winner from '../winnerboard';
 import logic from './logic';
 
-export function Board() {
-    const [turns, setTurns] = useState(0)
-    const [currentPlayer, setCurrentPlayer] = useState('x')
-    const [winner, setWinner] = useState(false)
-    const [combos, setCombos] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8])
-    const cells = []
+import './board.css';
 
-    const turn = () => setTurns(turns + 1)
-    const changePlayer = () => setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x')
+export function Board() {
+    const [turns, setTurns] = useState(0);
+    const [currentPlayer, setCurrentPlayer] = useState('x');
+    const [winner, setWinner] = useState(false);
+    const [combos, setCombos] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    const cells = [];
+
+    const turn = () => setTurns(turns + 1);
+    const changePlayer = () => setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x');
 
     const playerTurn = (index) => {
         const tmpCells = [...combos];
@@ -24,24 +25,24 @@ export function Board() {
         setCombos(tmpCells);
         logic(tmpCells, currentPlayer, setWinner);
         changePlayer(currentPlayer === 'x' ? 'o' : 'x');
-    }
+    };
 
-    
+
     for (let i = 0; i < 9; i++) {
         cells.push(<Cells player={currentPlayer} changePlayer={changePlayer} key={i} index={i} playerTurn={playerTurn} />)
     }
-    
+
     function resetGame() {
-        setTurns(0)
-        setCurrentPlayer('x')
-        setCombos([0, 1, 2, 3, 4, 5, 6, 7, 8])
-        setWinner(false)
+        setTurns(0);
+        setCurrentPlayer('x');
+        setCombos([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        setWinner(false);
     }
 
-    return ( 
-        !winner ? 
-        <div className='board'> {[...cells]} </div> 
-        : 
+    return (
+        !winner ?
+        <div className='board'> {[...cells]} </div>
+        :
         <Winner winner={currentPlayer === 'x' ? 'o' : 'x'} toWinTurns={turns} reset={resetGame}/>
         )
 };
